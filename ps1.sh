ee82bc6080d4fab9d8ffabfbad8c50d8f9a79ba4
rm76 <glauco@tutanota.de>
2025-07-14 18:49:39 +0200

    Update ps1.sh

--- ps1.sh
+++ ps1.sh
@@ -7,6 +7,16 @@ MAGENTA=$(tput setaf 5)
 CYAN=$(tput setaf 6)
 WHITE=$(tput setaf 7)
 RESET=$(tput sgr0)
+
+RED='\e[0;31m'
+GREEN='\e[0;32m'
+YELLOW='\e[0;33m'
+BLUE='\e[0;34m'
+MAGENTA='\e[0;35m'  # Purple
+CYAN='\e[0;36m'
+WHITE='\e[0;37m'
+RESET='\e[0m'
+
 PS1="${GREEN}\u${RESET} @ ${CYAN}\h${RESET} : ${YELLOW}\w${RESET} ${MAGENTA}\$${RESET} "
 
 # History
