1b671f5d9e7635704e6d798313ff56a070bf2848
rm76 <glauco@tutanota.de>
2025-07-14 17:58:30 +0200

    Update ps1.sh

--- ps1.sh
+++ ps1.sh
@@ -1,3 +1,4 @@
+# PS1 prompt
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 YELLOW=$(tput setaf 3)
@@ -7,3 +8,13 @@ CYAN=$(tput setaf 6)
 WHITE=$(tput setaf 7)
 RESET=$(tput sgr0)
 PS1="${GREEN}\u${RESET} @ ${CYAN}\h${RESET} : ${YELLOW}\w\n ${MAGENTA}\$${RESET} "
+
+# History
+HISTCONTROL="ignoredups:ignorespace"
+HISTIGNORE="history"
+HISTSIZE=1000
+HISTTIMEFORMAT="%Y/%m/%d %H:%M:%S "
+
+# Aliases
+alias ls="ls -h --color --time-style=long-iso"
+alias last="last -n 10 -Fwd"
